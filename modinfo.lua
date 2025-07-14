30420c59514d0837bf21968d969b3c0e086f90f8
Tian <66271788+TianYu-00@users.noreply.github.com>
2025-07-14 16:12:12 +0100

    Update modinfo.lua

--- modinfo.lua
+++ modinfo.lua
@@ -7,13 +7,13 @@ description = not isCN and [[
 󰀅 Lazy Wortox 󰀅
 
 Make playing Wortox easier with these convenient features:
-- Press a key to quickly drop souls from your inventory
-- Press a key to open soul jars effortlessly
-- Press a key to perform a leap in place
-- Press a key to withdraw souls from your soul jars
-- Press a key to store souls from your inventory into jars
-- Press a key to leap towards mouse position
-- Press a key to show range indicators for soul hop and heal
+· Press a key to quickly drop souls from your inventory
+· Press a key to open soul jars effortlessly
+· Press a key to perform a leap in place
+· Press a key to withdraw souls from your soul jars
+· Press a key to store souls from your inventory into jars
+· Press a key to leap towards mouse position
+· Press a key to show range indicators for soul hop and heal
 
 You can customize all key bindings and settings in the mod options menu.
 ]]
@@ -22,13 +22,13 @@ or
 󰀅 慵懒小恶魔 󰀅
 
 本模组为Wortox带来便捷功能：
-- 一键快速丢弃背包中的灵魂
-- 一键打开灵魂罐
-- 一键实现原地跳跃
-- 一键从灵魂罐中取出灵魂
-- 一键将物品栏的灵魂存入灵魂罐
-- 一键向鼠标位置跳跃
-- 一键显示灵魂跳跃与治疗范围指示圈
+· 一键快速丢弃背包中的灵魂
+· 一键打开灵魂罐
+· 一键实现原地跳跃
+· 一键从灵魂罐中取出灵魂
+· 一键将物品栏的灵魂存入灵魂罐
+· 一键向鼠标位置跳跃
+· 一键显示灵魂跳跃与治疗范围指示圈
 
 所有按键和设置均可在模组选项中自定义。
 ]]
